be6d999039dec0d3df0e755518673797b5d52280
techgalary <139435755+techgalary@users.noreply.github.com>
2026-04-03 13:03:34 +0530

    Update germany_bot.py

--- germany_bot.py
+++ germany_bot.py
@@ -230,9 +230,16 @@ async def run_bot():
 # -------------------------------
 # CLOUD RUN ENTRY (HTTP)
 # -------------------------------
+from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def run():
     asyncio.run(run_bot())
     return "Bot executed!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
